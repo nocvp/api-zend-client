69d53e1b37625518337987544015e373847f2a06
Semih Sarı <semih.sari@noc.com.tr>
2016-05-09 17:26:13 +0300

    feat(product): add product module

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -8,15 +8,29 @@
 
 namespace NocVpClient\Service;
 
+use NocVpClient\Model\Response\Product;
+use Zend\Http\Request;
+use Zend\Json\Json;
+
 class ProductService extends AbstractRequest
 {
     public function fetch($id)
     {
+        $response = $this->request('/v1/product/' . $id, Request::METHOD_GET);
 
+        return new Product($response->getBody());
     }
 
     public function fetchAll(array $params = array())
     {
+        $response = $this->request('/v1/product', Request::METHOD_GET);
+        $data = Json::decode($response->getBody(), Json::TYPE_ARRAY);
 
+        $results = array();
+        foreach($data as $row) {
+            $results[] = new Product($row);
+        }
+
+        return $results;
     }
 }
