900ea2f2b520c63713a441db0d6454be6dbea4b4
Semih Sarı <semih.sari@noc.com.tr>
2016-08-08 15:15:03 +0300

    Add asset response model getters and setters

--- src/Model/Response/Asset.php
+++ src/Model/Response/Asset.php
@@ -54,4 +54,132 @@ class Asset extends AbstractResponse
     {
         parent::__construct($data);
     }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSid()
+    {
+        return $this->sid;
+    }
+
+    /**
+     * @param string $sid
+     */
+    public function setSid($sid)
+    {
+        $this->sid = $sid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string $tags
+     */
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConversions()
+    {
+        return $this->conversions;
+    }
+
+    /**
+     * @param array $conversions
+     */
+    public function setConversions($conversions)
+    {
+        $this->conversions = $conversions;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreationTime()
+    {
+        return $this->creationTime;
+    }
+
+    /**
+     * @param int $creationTime
+     */
+    public function setCreationTime($creationTime)
+    {
+        $this->creationTime = $creationTime;
+    }
+
+    /**
+     * @return int
+     */
+    public function getModificationTime()
+    {
+        return $this->modificationTime;
+    }
+
+    /**
+     * @param int $modificationTime
+     */
+    public function setModificationTime($modificationTime)
+    {
+        $this->modificationTime = $modificationTime;
+    }
 }
